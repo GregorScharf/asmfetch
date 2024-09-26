646497020d6cacb1c055af1231793ade7eca7208
greg <aether@pop-os.fritz.box>
2024-09-26 21:38:00 +0200

    i am so fucking done

--- main.s
+++ main.s
@@ -3,6 +3,8 @@
 
 .equ BUFSIZE, 19
 
+.equ BITHACK, 10000
+
 newline:
   .asciz "\n"
 slash:
@@ -12,6 +14,45 @@ usedmem:
 path:
   .asciz "/proc/meminfo"
 
+# rdi is the base
+# rsi is the exponent
+pow:
+  push rbp
+  mov rbp, rsp
+  sub rsp, 24
+  mov QWORD PTR[rbp-8], rdi
+  mov QWORD PTR [rbp- 16], 0
+  mov QWORD PTR[rbp-24], rdx
+  mov rax, rdi
+  sub rsi, 1
+  jmp powL1
+
+powL1:
+  cmp QWORD PTR[rbp-16], rsi
+  jle powL2
+
+  jmp leave_pow
+  
+
+powL2:
+  mul QWORD PTR [rbp-8]
+  add QWORD PTR[rbp-16], 1
+
+  jmp powL1
+
+leave_pow:
+  xor rdx, rdx
+  mov rbx, 10
+  div rbx
+
+  mov rdx, QWORD PTR[rbp-24]
+  add rsi, 1
+
+  add rsp, 24
+  pop rbp
+  ret
+
+
 _start:
   call sysinfo
 
@@ -20,7 +61,6 @@ _start:
   syscall
 
 # rdi is an unsigned long as the argument
-# rax contains 
 its:
   push rbp
   mov rbp, rsp
@@ -42,19 +82,19 @@ its:
 
   mov rbx, BUFSIZE # i = BUFSIZE -1
 
-  jmp L1
+  jmp itsL1
 
-L1:
+itsL1:
   xor rax, rax
   xor rdx, rdx
   mov rax, QWORD PTR [rbp - 8] #
   mov rcx, 10                  #
   div rcx                      #
-  add edx, 48                  #
+  add rdx, 48                  #
 
 
   mov rdi, r10                 # str[i] = result from the above
-  add rdi, rbx                 # 
+  add rdi, rbx                 #
   mov byte [rdi], dl           #
   sub rbx, 1                   # i--
 
@@ -64,7 +104,7 @@ L1:
   je leave_its
 
   cmp rbx, 0
-  jne L1
+  jne itsL1
 
   jmp leave_its
 
@@ -81,27 +121,26 @@ strlen:
   mov rbp, rsp
   sub rsp, 8
   mov QWORD PTR[rbp -8], 0 # long n = 0
-  jmp L2
+  jmp strlenL1
   
 
-L2:
+strlenL1:
   mov rsi, rdi
   add rsi, QWORD PTR[rbp-8]
   mov dl, byte[rsi]
   cmp dl, 0
-  jne L3
+  jne strlenL2
 
   jmp leave_strlen
 
 leave_strlen:
   mov rax, QWORD PTR [rbp-8]
-  add rax, 1
   add rsp, 8
   pop rbp
   ret
-L3:
+strlenL2:
   add QWORD PTR[rbp-8], 1
-  jmp L2
+  jmp strlenL1
 
 # rdi is the char* argument
 sti:
@@ -110,22 +149,26 @@ sti:
   sub rsp, 40 # 40 + 8 from rbp, 8 bytes wasted
   call strlen
   sub rax, 1
-  mov QWORD PTR [rbp - 8], rax
-  mov QWORD PTR [rbp - 16], 0
-  mov QWORD PTR [rbp - 24], 0
-  mov QWORD PTR [rbp - 32], 0
-  mov QWORD PTR [rbp - 40], rdi #save pointer to stack
-  jmp L4
 
-L4:
+  cmp rax, 17
+  jae error
+
+  mov QWORD PTR [rbp - 8], rax
+  mov QWORD PTR [rbp - 16], 0 # result value = 0
+  mov QWORD PTR [rbp - 24], 0 # long expo = 0
+  mov QWORD PTR [rbp - 32], 0 # long i = 0
+  mov QWORD PTR [rbp - 40], rdi #save pointer to stack
+  jmp stiL1
+
+stiL1:
   cmp QWORD PTR [rbp-32], rax
-  jle L5
+  jle stiL2
 
   mov QWORD PTR [rbp-32], rax # i = length of str - 1
-  # TODO: finsish logic for str to int conversion !here
+  add QWORD PTR [rbp-32], BITHACK
   jmp cont_sti
 
-L5:
+stiL2:
   mov rsi, rdi
   add rsi, QWORD PTR[rbp-32] # str[i]
   mov dl, byte [rsi]
@@ -136,10 +179,45 @@ L5:
   jae error
 
   add QWORD PTR[rbp-32], 1
-  jmp L4
+  jmp stiL1
 
 cont_sti:
-  
+  cmp QWORD PTR [rbp-32], BITHACK-1 # i >= 0
+  jae stiL3
+
+  jmp leave_sti
+
+stiL3:
+  xor rdx, rdx
+
+  mov rsi, QWORD PTR [rbp - 40]
+  sub QWORD PTR[rbp-32], BITHACK
+  add rsi, QWORD PTR [rbp - 32]
+  add QWORD PTR[rbp-32], BITHACK
+  mov dl, byte[rsi]
+  sub dl, 48
+
+  mov rdi, 10
+  mov rsi, QWORD PTR[rbp-24]
+  call pow
+
+  mov rsi, rax
+  mov rax, rdx # (u64)dl
+  xor rdx, rdx
+  mul rsi
+
+  add QWORD PTR[rbp-16], rax
+
+  sub QWORD PTR [rbp-32], 1 # i--;
+  add QWORD PTR [rbp-24], 1 # expo++;
+
+  jmp cont_sti
+
+leave_sti:
+  mov rax, QWORD PTR[rbp-16]
+  add rsp, 40
+  pop rbp
+  ret
 
 error:
   mov rax, 60
@@ -170,6 +248,44 @@ sysinfo:
   add rdi, 8
   mov rsi, [rdi] # free memory
 
+  sub rax, rsi
+  mov QWORD PTR[rbp-128], rax
+ #########################################################
+  mov rsi, 256
+  mov rax, 9
+  mov rdi, 0
+  mov rdx, 3
+  mov r10, 34
+  mov r8, -1
+  mov r9, 0
+  syscall
+  mov QWORD PTR[rbp - 120], rax
+
+  mov rax, 2
+  lea rdi, path
+  mov rsi, 0
+  mov rdx, 0777
+  syscall
+
+  mov rdi, rax
+  mov rax, 0
+  mov rsi, QWORD PTR [rbp-120]
+  mov rdx, 136
+  syscall
+
+  add QWORD PTR[rbp-120], 129
+
+  mov rdi, QWORD PTR[rbp-120]
+
+  call sti
+
+  xor rdx, rdx
+  mov rbx, 1000
+  mul rbx
+########################################################
+
+  mov rsi, rax
+  mov rax, QWORD PTR[rbp-128]
   sub rax, rsi
 
   xor rdx, rdx
@@ -234,40 +350,6 @@ sysinfo:
   lea rsi, newline
   mov rdx, 1
   syscall
-
-  
-  mov rsi, 256
-  mov rax, 9
-  mov rdi, 0
-  mov rdx, 3
-  mov r10, 34
-  mov r8, -1
-  mov r9, 0
-  syscall
-  mov QWORD PTR[rbp - 120], rax
-
-  mov rax, 2
-  lea rdi, path
-  mov rsi, 0
-  mov rdx, 0777
-  syscall
-
-  mov rdi, rax
-  mov rax, 0
-  mov rsi, QWORD PTR [rbp-120]
-  mov rdx, 136
-  syscall
-
-  add QWORD PTR[rbp-120], 129
-
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, QWORD PTR[rbp-120]
-  mov rdx, 250
-  syscall
-
-  mov rdi, rsi
-  call strlen
 
   add rsp, 128
   pop rbp
--- test.c
+++ test.c
@@ -16,10 +16,18 @@ unsigned long my_strlen(const char *str) {
   return n;
 }
 
+unsigned long my_pow(unsigned long base, unsigned long expo) {
+  unsigned long ori = base;
+  for (int i = 0; i < expo; i++) {
+    base = ori * base;
+  }
+  return base / 10;
+}
+
 unsigned long strToInt(const char *str) {
   long len = my_strlen(str);
   unsigned long num = 0;
-  long iter = 0;
+  long expo = 0;
   for (long i = 0; i < len; i++) {
     if (str[i] < 48 || str[i] > 57) {
       printf("you're dumb \"%c\" is not a number", str[i]);
@@ -29,8 +37,8 @@ unsigned long strToInt(const char *str) {
 
   for (long i = len - 1; i >= 0; i--) {
 
-    num += (str[i] - 48) * pow(10, iter);
-    iter++;
+    num += (str[i] - 48) * my_pow(10, expo);
+    expo++;
   }
 
   return num;
